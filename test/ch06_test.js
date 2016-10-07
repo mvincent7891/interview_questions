d5c409c143e7e4c02bcca82b78c8723017e34157
Michael Parlato <michaelvparlato@gmail.com>
2016-10-07 16:12:41 -0700

    finished chapter 6

---
+++ test/ch06_test.js
@@ -0,0 +1,58 @@
+import { expect } from 'chai';
+import { dutchFlag, stockPicker,
+         randomSubsets, spiralOrdering } from '../lib/ch06.js';
+
+describe("Chapter 6 Question", function () {
+
+  describe("6.1 #dutchFlag", function() {
+    it("works with an example case", function() {
+      var arr  = [6, 3, 3, 4, 2, 1, 2, 2, 3, 8, 7, 6, 5, 3];
+      var result = dutchFlag(arr, 1);
+      expect(result).to.equal([2, 1, 2, 2, 3, 3, 3, 3, 6, 8, 7, 6, 5, 4]);
+    });
+  });
+
+  describe("6.6 #stockPicker", function() {
+    it("does not settle for a local max", function() {
+      var arr  = [13, 7, 12, 8, 13];
+      var result = stockPicker(arr);
+      expect(result).to.equal([1, 4]);
+    });
+
+    it("picks the right days", function() {
+      var arr  = [13, 7, 12, 5, 13];
+      var result = stockPicker(arr);
+      expect(result).to.equal([3, 4]);
+    });
+  });
+
+  describe("6.11 #randomSubsets", function () {
+    it("generates subsets of the appropriate length", function () {
+      let arr = Array.from({length: 100}, (k,v) => k);
+      let result = randomSubsets(arr, 10);
+      expect(result.length).to.equal(10);
+    });
+
+    it("does not generate the same subsetfor identical inputs", function() {
+      let arr1 = Array.from({length: 100}, (k,v) => k);
+      let result1 = randomSubsets(arr1, 10);
+      let result2 = randomSubsets(arr1, 10);
+      expect(result1).to.not.equal(result2);
+    });
+  });
+
+  describe("6.17 #spiralOrdering", function () {
+    it("works with a 3x3 array", function () {
+      let matrix = [[1,2,3],[4,5,6],[7,8,9]];
+      let result = spiralOrdering(matrix);
+      expect(result).to.equal([1,2,3,6,9,8,7,4,5]);
+    });
+
+    it("works with a 3x3 array", function () {
+      let matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
+      let result = spiralOrdering(matrix);
+      expect(result).to.equal([1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]);
+    });
+  });
+
+});
