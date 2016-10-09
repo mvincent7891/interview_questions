8a5b831bdcf69525bed231ef01e23e84b1f0e695
Michael Parlato <michaelvparlato@gmail.com>
2016-10-09 15:08:15 -0700

    chapter 11 tests complete

--- test/ch11_test.js
+++ test/ch11_test.js
@@ -1,22 +1,34 @@
 import { expect } from 'chai';
 import {sortedArrSort, kthLargest} from '../lib/ch11.js';
 
-// TODO: write tests for chapter 11
-
 describe("Chapter 11 Questions", () => {
 
- describe("#sortedArrSort", () => {
+ describe("11.1 #sortedArrSort", () => {
 
-   it("", function () {
+   it("merges sorted arrays", function () {
+     let a = [1, 3, 5];
+     let b = [2, 4, 6];
+     let c = [0];
 
+     let result = sortedArrSort(a, b, c);
+     expect(result).to.equal([0,1,2,3,4,5,6]);
    });
 
  });
 
  describe("#kthLargest", () => {
 
-   it("", function () {
+   var tests = [
+     {arr: [1,2,3,4,5,6,7], k: 3, nth:  '3rd', expected: 5},
+     {arr: [1,2,3,4,5,6,7,8], k: 3, nth: '3rd', expected: 6},
+     {arr: [12,4,5,6,2,3,8,10,1,13], k: 2, nth: '2nd', expected: 12}
+   ];
 
+   tests.forEach(test => {
+     it(`picks the ${test.nth} largest element from ${test.arr.length} elements`, () => {
+       let result = kthLargest(test.args, test.k);
+       expect(result).to.equal(test.expected);
+     });
    });
 
  });
