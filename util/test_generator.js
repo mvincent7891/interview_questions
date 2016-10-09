661be5f6843ea2ffb23c22393bdf996bdd93c9b1
Michael Parlato <michaelvparlato@gmail.com>
2016-10-08 21:21:56 -0700

    Finished testGenerator

--- util/test_generator.js
+++ util/test_generator.js
@@ -1,22 +1,56 @@
+// 1. Change chapterNumber below before using
+// 2. Navigate to root directory and execute the following:
+//      node --harmony 'util/test_generator'
+// 3. Uncomment the tests in the newly created file
+
 var writeFile = require('write');
 
 const insertIt = string => {
   string += `\n\/\/    it(\"\", function () {\n`;
-  string += `\n\/\/    });`;
+  string += `\n\/\/    });\n`;
+  return string;
+};
+
+const insertDescribe = (string, name) => {
+  string += `\n\/\/  describe(\"#${name}\", () => {\n`;
+  string = insertIt(string);
+  string += `\n\/\/  });\n`;
   return string;
 };
 
 let chapterNumber = 11;
 
+if (!chapterNumber) {
+  throw(`Change the chapter number, foo!`);
+}
+
+var testSubjects = require(`../lib/ch${chapterNumber}.js`);
+
+const importSubjects = string => {
+  string += `import {`;
+  let names = [];
+  Object.keys(testSubjects).forEach(function (subject) {
+    names.push(subject.toString());
+  });
+  string += names.join(', ');
+  string += `} from \'../lib/ch${chapterNumber}.js\';\n\n`;
+  return string;
+};
+
 let textToWrite = ``;
 
-textToWrite += `import { expect } from \'chai\';\n\n`;
+textToWrite += `import { expect } from \'chai\';\n`;
+
+textToWrite = importSubjects(textToWrite);
 
 textToWrite += `\/\/describe(\"Chapter ${chapterNumber} Questions\", () => {\n`;
 
-textToWrite = insertIt(textToWrite);
+Object.keys(testSubjects).forEach(function (subject) {
+  let methodName = subject.toString();
+  textToWrite = insertDescribe(textToWrite, methodName);
+});
 
-textToWrite += `\/\/\n});`;
+textToWrite += `\n\/\/});`;
 
 
 writeFile(`./test/ch${chapterNumber}_test.js`, `${textToWrite}`);
