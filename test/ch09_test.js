eca74f8231a17802c8dc5f9fed1a9f3f99a96170
Michael Parlato <michaelvparlato@gmail.com>
2016-10-08 18:32:18 -0700

    finished chapter 9 specs

--- test/ch09_test.js
+++ test/ch09_test.js
@@ -24,16 +24,47 @@ describe("Chapter 9 Questions", function () {
 
   describe("9.3 #isWellFormed", function () {
 
-    it("", function () {
+    it("returns true for a well-formed case", function () {
+      let result = isWellFormed('([]){()}');
+      expect(result).to.be.true;
+    });
 
+    it("returns false for an ill-formed case", function () {
+      let result = isWellFormed('([]){()');
+      expect(result).to.be.false;
     });
 
   });
 
   describe("9.7 #treeLevels", function () {
 
-    it("", function () {
+    it("works with a sparse tree", function () {
+      let a = new TreeNode(3);
+      let b = new TreeNode(1);
+      let c = new TreeNode(4);
+      a.right = b;
+      b.right = c;
 
+      let result = treeLevels(a);
+      expect(result).to.equal([[3],[1],[4]]);
+    });
+
+    it("works with a full tree", function () {
+      let a = new TreeNode(314)
+      let b = new TreeNode(6)
+      let c = new TreeNode(6)
+      let d = new TreeNode(5)
+      let e = new TreeNode(7)
+      let f = new TreeNode(8)
+
+      a.left = b;
+      a.right = c;
+      b.left = d;
+      b.right = e;
+      c.left = f;
+
+      let result = treeLevels(a);
+      expect(result).to.equal([[314],[6,6],[5,7,8]])
     });
 
   });
